91350567215277fd62675afe209445c9169c5e09
ahaxi <yixifu23@gmail.com>
2014-07-05 22:56:34 -0700

    Create NLP_commands.sh

---
+++ NLP_commands.sh
@@ -0,0 +1,7 @@
+tr ‘A-Z’ ‘a-z’ < shakes.txt | tr -sc ‘A-Za-z’ ‘\n’ | sort | uniq -c | sort -n -r
+
+tr ‘A-Z’ ‘a-z’ < shakes.txt | tr -sc ‘A-Za-z’ ‘\n’ | grep ‘[aeiou].*ing$’ | sort | uniq -c
+
+# tr means translate
+# s means compress
+# c mean complement opposite to what specified

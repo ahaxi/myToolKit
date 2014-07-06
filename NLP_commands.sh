5549df0ecefb0f9d85280148917005b84084d5c2
ahaxi <yixifu23@gmail.com>
2014-07-05 23:23:47 -0700

    Update NLP_commands.sh

--- NLP_commands.sh
+++ NLP_commands.sh
@@ -1,3 +1,5 @@
+## Some commands that I came across while learning on NLP Stanford Class on Coursera
+
 tr ‘A-Z’ ‘a-z’ < shakes.txt | tr -sc ‘A-Za-z’ ‘\n’ | sort | uniq -c | sort -n -r
 
 tr ‘A-Z’ ‘a-z’ < shakes.txt | tr -sc ‘A-Za-z’ ‘\n’ | grep ‘[aeiou].*ing$’ | sort | uniq -c
